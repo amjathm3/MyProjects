a9260389283552cac0e77e8b83b58d7c54395a21
amjathm3 <amjathm3@gmail.com>
2026-07-21 20:45:22 +0530

    added git ignore  2026 copyright updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <p>&copy; 2025 Amjath Ali M. All rights reserved.</p>
+        <p>&copy; 2026 Amjath Ali M. All rights reserved.</p>
       </div>
     </footer>
   )
